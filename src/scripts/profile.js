0aa191b12ced84826fbfaa4a0b902789f01d3e37
paulabeatrizz <paula.beatriz@upe.br>
2024-12-02 21:11:51 -0300

    feat: logica inicial de mudar foto do banner

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -13,3 +13,19 @@ function updateProfile() {
         reader.readAsDataURL(file);
     }
 }
+
+function updateBanner() {
+    const input = document.getElementById("bannerInput");
+    const bannerImg = document.querySelector(".banner-conteudo img");
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = function () {
+        bannerImg.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file); 
+    }
+}
